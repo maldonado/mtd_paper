29f43bdd3a0404e96948f26dc9d78afe91be7926
Everton Maldonado <everton.maldonado@gmail.com>
2015-07-10 17:12:00 -0400

    change technical debt distribution figure to bw version

--- r_scripts/r_script.r
+++ r_scripts/r_script.r
@@ -1,7 +1,7 @@
 
 mydata <- data.frame(Ant=c(70.89,11.94,9.7,7.46,0), Jmeter=c(84.26,5.86,5.86,3.2,0.8), ArgoUml=c(48.45,39.38,7.68,2.66,1.81), JFreeChart=c(84.01,11.41,4.10,0.45,0), Columba=c(42.71,45.71,4.40,2.03,5.42))
-barplot(as.matrix(mydata),  ylab="Percentage",  ylim=c(0, 100), beside=TRUE, col=1, density=c(28,23,23,18,18), angle = c(0, 50, 90, 140, 170))
-legend(23, 92, c("Design debt", "Requirement debt", "Defect debt","Test debt", "Documentation debt"), cex=1.3, col=1, density=c(28,23,23,18,18), angle = c(0, 50, 90, 140, 170) , bty="y", seg.len=0.5)
+barplot(as.matrix(mydata),  ylab="Percentage",  ylim=c(0, 100), beside=TRUE, col=1, density=c(13,8,8,3,3), angle = c(0, 50, 90, 140, 180))
+legend(23, 92, c("Design debt", "Requirement debt", "Defect debt","Test debt", "Documentation debt"), cex=1.3, col=1, density=c(18,13,13,8,8), angle = c(0, 50, 90, 140, 180) , bty="y", seg.len=0.5)
 
 
 
